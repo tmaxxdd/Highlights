0293cdfe62639b52601f36ab1088461a63a8fa2b
tkadziolka <tmax0135@gmail.com>
2023-05-22 07:55:47 +0200

    Completed example for android

--- androidExample/src/main/java/pl/tkadziolka/androidexample/Dropdown.kt
+++ androidExample/src/main/java/pl/tkadziolka/androidexample/Dropdown.kt
@@ -1,5 +1,6 @@
 package pl.tkadziolka.androidexample
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -7,6 +8,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.MenuItemColors
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -15,6 +17,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
@@ -33,7 +36,7 @@ fun Dropdown(
 
     Column(Modifier.padding(16.dp)) {
         DropdownMenu(
-            modifier = Modifier,
+            modifier = Modifier.background(Color.White),
             expanded = isExpanded,
             onDismissRequest = { isExpanded = false },
         ) {
--- androidExample/src/main/java/pl/tkadziolka/androidexample/MainActivity.kt
+++ androidExample/src/main/java/pl/tkadziolka/androidexample/MainActivity.kt
@@ -7,14 +7,19 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import pl.tkadziolka.highlights.Highlights
 import pl.tkadziolka.highlights.model.SyntaxLanguage
 import pl.tkadziolka.highlights.model.SyntaxThemes
@@ -42,12 +47,14 @@ private val sampleCode =
 
 @Composable
 fun App() {
-    var highlights = remember {
-        Highlights
-            .default()
-            .getBuilder()
-            .code(sampleCode)
-            .build()
+    val highlights = remember {
+        mutableStateOf(
+            Highlights
+                .default()
+                .getBuilder()
+                .code(sampleCode)
+                .build()
+        )
     }
 
     MaterialTheme {
@@ -58,27 +65,36 @@ fun App() {
             horizontalAlignment = Alignment.Start,
             verticalArrangement = Arrangement.SpaceBetween
         ) {
-            CodeTextView(highlights = highlights)
-
-            Dropdown(
-                options = SyntaxLanguage.getNames(),
-                selected = SyntaxLanguage.getNames().indexOfFirst {
-                    it.equals(highlights.getLanguage().name, ignoreCase = true)
-                }) { selectedLanguage ->
-                highlights = highlights.getBuilder()
-                    .language(SyntaxLanguage.getByName(selectedLanguage)!!)
-                    .build()
-            }
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Highlights",
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center
+            )
 
             Spacer(modifier = Modifier.size(16.dp))
+
+            CodeTextView(highlights = highlights.value)
+
+            Spacer(modifier = Modifier.weight(1f))
 
             Dropdown(
                 options = SyntaxThemes.light.keys.toList(),
                 selected = SyntaxThemes.light.entries.indexOfFirst {
-                    it.value == highlights.getTheme()
+                    it.value == highlights.value.getTheme()
                 }) { selectedTheme ->
-                highlights = highlights.getBuilder()
+                highlights.value = highlights.value.getBuilder()
                     .theme(SyntaxThemes.light[selectedTheme]!!)
+                    .build()
+            }
+
+            Dropdown(
+                options = SyntaxLanguage.getNames(),
+                selected = SyntaxLanguage.getNames().indexOfFirst {
+                    it.equals(highlights.value.getLanguage().name, ignoreCase = true)
+                }) { selectedLanguage ->
+                highlights.value = highlights.value.getBuilder()
+                    .language(SyntaxLanguage.getByName(selectedLanguage)!!)
                     .build()
             }
         }
